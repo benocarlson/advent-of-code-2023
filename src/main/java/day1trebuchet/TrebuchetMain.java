e3f535e50a92c00bb46bc4de9740384ef2218ec4
Ben <benocarlson@gmail.com>
2023-12-01 10:23:33 -0700

    accept file from args
    the puzzle is done, but I just can't stop myself from making it a little nicer

--- src/main/java/day1trebuchet/TrebuchetMain.java
+++ src/main/java/day1trebuchet/TrebuchetMain.java
@@ -8,12 +8,17 @@ import java.io.FileNotFoundException;
 public class TrebuchetMain {
 
     private static final String DEFAULT_PART = "part1";
+    private static final String DEFAULT_FILE = "inputFiles/day1trebuchet.txt";
 
     public static void main(String[] args) {
 
         String part = DEFAULT_PART;
         if (args.length > 0) {
             part = args[0];
+        }
+        String fileName = DEFAULT_FILE;
+        if (args.length > 1) {
+            fileName = args[1];
         }
 
         ValueFinder valueFinder;
@@ -24,13 +29,12 @@ public class TrebuchetMain {
             default -> {
                 System.out.println("Invalid argument: " + part);
                 System.out.println("Valid arguments are: ");
-                System.out.println("\tpart1\t calculate based on numerals only");
-                System.out.println("\tpart2\t calculate based on numerals or spelled out numbers");
+                System.out.println("\tpart1 <filename>\t calculate based on numerals only");
+                System.out.println("\tpart2 <filename>\t calculate based on numerals or spelled out numbers");
                 return;
             }
         }
 
-        String fileName = "inputFiles/day1trebuchet.txt";
         TrebuchetCalculator calculator = new TrebuchetCalculator(valueFinder);
         try {
             System.out.println(calculator.calculateFromFile(fileName));
