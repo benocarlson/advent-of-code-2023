57aaa6bb05f3847063b2bff2b29456ed3c32e3a9
Ben <benocarlson@gmail.com>
2023-12-02 20:05:34 -0700

    solve part 1 in CubeMain

--- src/main/java/day2cubeconundrum/CubeMain.java
+++ src/main/java/day2cubeconundrum/CubeMain.java
@@ -1,7 +1,24 @@
 package day2cubeconundrum;
 
+import day2cubeconundrum.part1.CubeGameValidator;
+import utils.FileUtils;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
 public class CubeMain {
+    private static final String DEFAULT_FILE_NAME = "inputFiles/day2cubeconundrum";
     public static void main(String[] args) {
 
+        CubeGameValidator validator = new CubeGameValidator();
+
+        try {
+            List<CubeGame> cubeGames = FileUtils.getLinesFromFile(DEFAULT_FILE_NAME).stream()
+                    .map(CubeGameParser::parseStringAsCubeGame).toList();
+            int result = validator.sumValidGameIds(cubeGames);
+            System.out.println("Result: " + result);
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not find file: " + DEFAULT_FILE_NAME);
+        }
     }
 }
