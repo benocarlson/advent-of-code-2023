88ff0735bb8a1f563b338664ba589086e04bdd3b
Ben <benocarlson@gmail.com>
2023-12-02 20:23:46 -0700

    move main to part 1 package

--- src/main/java/day2cubeconundrum/part1/CubePart1Main.java
+++ src/main/java/day2cubeconundrum/part1/CubePart1Main.java
@@ -1,12 +1,14 @@
-package day2cubeconundrum;
+package day2cubeconundrum.part1;
 
+import day2cubeconundrum.CubeGame;
+import day2cubeconundrum.CubeGameParser;
 import day2cubeconundrum.part1.CubeGameValidator;
 import utils.FileUtils;
 
 import java.io.FileNotFoundException;
 import java.util.List;
 
-public class CubeMain {
+public class CubePart1Main {
     private static final String DEFAULT_FILE_NAME = "inputFiles/day2cubeconundrum";
     public static void main(String[] args) {
 
